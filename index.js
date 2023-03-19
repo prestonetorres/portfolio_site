fdf9950116ae0612b0a9dfb1c57fbc71fb996028
prestonetorres <prestonetorres@gmail.com>
2023-03-18 17:09:15 -0700

    Added JS functionality for button state, button fill animations

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const btns = btnContainer.getElementsByClassName("btn");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     let current = document.getElementsByClassName("active");
+    let currPage = document.querySelectorAll('nav a');
 
-    if (current.length > 0) {
+    if (current.length > 0 && currPage.getAttribute('href') === '#') {
       current[0].className = current[0].className.replace(" active", "");
     }
     this.className += " active";
   });
 }
+ 
